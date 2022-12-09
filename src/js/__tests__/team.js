7db6428e71e554b142dcb221c9ff23f2263b6092
Marko Melle <markmelle1@gmail.com>
2022-12-09 19:28:52 +0100

    test changed

--- src/js/__tests__/team.js
+++ src/js/__tests__/team.js
@@ -1,16 +1,8 @@
 import Team from '../team';
 
 test('should iterize the characters', () => {
-  const charsArr = [
-    {
-      name: 'Демон', type: 'Demon', health: 50, level: 1, attack: 40, defence: 10,
-    },
-    {
-      name: 'Лучник', type: 'Bowman', health: 50, level: 2, attack: 40, defence: 10,
-    },
-  ];
   const received = [];
-  const team = new Team(charsArr);
+  const team = new Team();
   for (const char of team) {
     received.push(char);
   }
