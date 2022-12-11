ba9a571551a81cda7bd88574c5cfa0c2d6d47e4a
Marko Melle <markmelle1@gmail.com>
2022-12-11 20:58:48 +0100

    add Version 2

--- src/js/__tests__/team.js
+++ src/js/__tests__/team.js
@@ -1,16 +1,19 @@
+import Character from '../char';
 import Team from '../team';
 
 test('should iterize the characters', () => {
   const received = [];
   const team = new Team();
+  team.addChar(new Character('Демон', 'Demon'));
+  team.addChar(new Character('Лучник', 'Bowman'));
   for (const char of team) {
     received.push(char);
   }
   const expected = [{
-    name: 'Демон', type: 'Demon', health: 50, level: 1, attack: 40, defence: 10,
+    name: 'Демон', type: 'Demon', health: 100, level: 1, attack: 40, defence: 10,
   },
   {
-    name: 'Лучник', type: 'Bowman', health: 50, level: 2, attack: 40, defence: 10,
+    name: 'Лучник', type: 'Bowman', health: 100, level: 1, attack: 40, defence: 10,
   }];
   expect(received).toEqual(expected);
 });
--- src/js/team.js
+++ src/js/team.js
@@ -1,18 +1,15 @@
 /* eslint-disable no-plusplus */
 export default class Team {
-  constructor() {
-    this.char0 = {
-      name: 'Демон', type: 'Demon', health: 50, level: 1, attack: 40, defence: 10,
-    };
-    this.char1 = {
-      name: 'Лучник', type: 'Bowman', health: 50, level: 2, attack: 40, defence: 10,
-    };
+  #counter = 0;
+
+  addChar(char) {
+    this[`char${this.#counter++}`] = char;
   }
 
   [Symbol.iterator]() {
     const thisObj = this;
     let current = 0;
-    const last = 2;
+    const last = this.#counter;
     return {
       next() {
         const id = `char${current}`;
