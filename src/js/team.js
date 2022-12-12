775cdb468d9cdb0a44d60d41c198f488fa34033d
Marko Melle <markmelle1@gmail.com>
2022-12-12 17:16:08 +0100

    team modified

--- src/js/team.js
+++ src/js/team.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-plusplus */
 export default class Team {
   constructor() {
-    this.Chars = [];
+    this.chars = [];
   }
 
   addChar(char) {
-    this.Chars.push(char);
+    this.chars.push(char);
   }
 
   [Symbol.iterator]() {
     let current = 0;
-    const last = this.Chars.length;
+    const last = this.chars.length;
     return {
       next: () => {
         if (current < last) {
-          return { value: this.Chars[current++], done: false };
+          return { value: this.chars[current++], done: false };
         }
         return { done: true };
       },
